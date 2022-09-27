c78cab75d70f5a5df303feb2a9b110e25e20334a
Pushpa Padti <99261071+ppadti@users.noreply.github.com>
2022-09-27 12:40:31 +0530

    Update AddToForm.tsx

--- todoapp/src/Components/AddToForm.tsx
+++ todoapp/src/Components/AddToForm.tsx
@@ -4,7 +4,19 @@ import {
   Button,
   Form,
   FormGroup,
+  TextContent,
   TextInput,
+  TextVariants,
+  Text,
+  PageSectionVariants,
+} from '@patternfly/react-core'
+
+import {
+  Page,
+  Masthead,
+  MastheadMain,
+  MastheadBrand,
+  PageSection,
 } from '@patternfly/react-core'
 
 interface Props {
@@ -23,27 +35,47 @@ const AddToForm = ({ handleAdd }: Props) => {
     setTodo(task)
   }
 
+  const header = (
+    <Masthead>
+      <MastheadMain>
+        <MastheadBrand href="https://patternfly.org" target="_blank">
+          <TextContent
+            style={{ textAlign: 'center', fontWeight: 'bold', padding: '2rem' }}
+          >
+            <Text component={TextVariants.h2}>TODO LIST</Text>
+          </TextContent>
+        </MastheadBrand>
+      </MastheadMain>
+    </Masthead>
+  )
+
   return (
     <>
-      <Form isHorizontal style={{ justifyContent: 'center' }}>
-        <TextInput
-          style={{ width: '25rem' }}
-          value={todo}
-          placeholder="Enter a task"
-          isRequired
-          type="text"
-          id="horizontal-form-name"
-          aria-describedby="horizontal-form-name-helper"
-          name="horizontal-form-name"
-          onChange={handleChange}
-        />
-
-        <ActionGroup>
-          <Button variant="primary" type="submit" onClick={addTask}>
-            Add
-          </Button>
-        </ActionGroup>
-      </Form>
+      <Page header={header}>
+        <PageSection
+          isWidthLimited
+          isCenterAligned
+          variant={PageSectionVariants.light}
+        >
+          <Form isWidthLimited>
+            <FormGroup label="Task" isRequired fieldId="0-label1">
+              <TextInput
+                isRequired
+                id="0-label1"
+                name="0-label1"
+                value={todo}
+                placeholder="Enter a task"
+                onChange={handleChange}
+              />
+            </FormGroup>
+            <ActionGroup>
+              <Button variant="primary" type="submit" onClick={addTask}>
+                Add
+              </Button>
+            </ActionGroup>
+          </Form>
+        </PageSection>
+      </Page>
     </>
   )
 }
